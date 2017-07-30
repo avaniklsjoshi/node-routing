0d74d0880a1ce75181a4a0354336ac162b7446bb
Avani joshi <avaniklsjoshi@gmail.com>
2017-07-30 20:38:24 +0530

    finalyzed

--- server.js
+++ server.js
@@ -5,47 +5,8 @@ var app=require('express')(),
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(authenticate);
 
-
-app.get('/',function(req,res){
-  res.sendFile(__dirname+'/index.html');
-});
-app.get('/about',function(req,res){
-  res.json({message:'about!'});
-});
-
-app.route('/contact')
-  .get(function(req,res){
-    res.sendFile(__dirname+'/contact.html');
-  })
-  .post(function(req,res){
-    console.log(req.body);
-    res.send('hello' +req.body.name+'! Nice to meet you');
-  });
-
-app.get('/@:username/:post_slug', checkName, function(req, res){
-  console.log(req.params);
-  res.send('You are reading ' + req.params.post_slug + ' by ' + req.params.username);
-});
-
-function checkName(req, res, next){   //used for authentication
-  console.log(req.params+'this is middleware');
-
-  // validation
-  // check the DB
-  // var user=User.findOne({username:req.params.username});
-  // if(!user)
-
-  next();
-}
-function authenticate(req,res,next){
-  // make sure the user is authenticated
-  // req.params.token
-
-  console.log('authenticating user..');
-  next();
-}
+app.use(require('./app/routes'));
 
 app.listen(port,function(){
   console.log('app listening on '+port);
